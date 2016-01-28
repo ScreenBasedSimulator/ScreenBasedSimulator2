18df3fe0150d79e4cc4249cbe0fa26ac918fe89a
Rayenkang <yuekang@andrew.cmu.edu>
2016-01-27 22:00:37 -0500

    remove newSBS

--- unity/GunFire.js
+++
@@ -1,7 +0,0 @@
-function Update() {
-	if(Input.GetButtonDown("Fire1")) {
-		var gunsound : AudioSource = GetComponent.<AudioSource>();
-		gunsound.Play();
-		GetComponent.<Animation>().Play("GunShot");
-	}
-}
