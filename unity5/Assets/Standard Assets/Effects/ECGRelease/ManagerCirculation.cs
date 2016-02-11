0d5df2f6c4d254df570000f395586d2c9432bd12
machongshen <machongshen@gmail.com>
2016-02-11 14:08:41 -0500

    new curve added

---
+++ unity5/Assets/Standard Assets/Effects/ECGRelease/ManagerCirculation.cs
@@ -0,0 +1,284 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 	This script works out the voltage output for the heart as the various chambers relax and contract.
+//	It also makes the audio beep for every heartbeat which can easily be removed or commented out (found in:= public IEnumerator QRSWave)
+//	This voltage is then read from the tracemesh script to plot the Y component of the trace with X being the time.
+//
+//	To change the heart rate then reduce: intervalP - a 1.0f interval will give you a heartrate of 60 beats per sec which is a default normal resting rate
+//	Changing it down to 0.5f will give you a heartrate of 120 which should realistically be considered the top end of what you'd expect to normally reach
+//	during periods of stress and exertion. Any rate highter than this will start to break the standard wave form up and you would get merging of the various
+//	waves between each heartbeat - it is worth noting that the QRS (the big spike) and the interval to the next rise after that do decrease with higher heartrates
+//	but generally the waveform should visualise as a consistent shape across the various heartrates with the variation between the spacing between each waveform.
+
+
+public class ManagerCirculation : MonoBehaviour {
+
+	//	The sound files that we use
+
+	// public AudioSource
+	// public AudioSource
+
+	//	Trigger variables for the stimulation of the nodes which control the pace setting of the hearts electical conduction system
+	
+	public bool triggerSANode = false; 				// 	Whether we need to stimulate the atria
+	private bool triggerAVNode = false; 			// 	Whether we need to stimulate the ventricles
+	public bool playSound = true;					//	Whether we have sound with the trace
+	public bool stateSystole = false;				//	Whether the heart is working (true) or stopped (false)
+
+	//	Heart voltage values for the formulation of the ECG trace
+
+	public float voltageSum;						// 	Sum voltage measured
+	public float voltageSumPrevious;				// 	Last sum voltage recorded
+	private float voltageP;							// 	Atrial contraction voltage
+	private float voltageQRS;						// 	Ventricular contraction
+	private float voltageT;							// 	Ventricular relaxation (repolarisation)
+	private float voltageU;							// 	Purkinjee repolarisation
+	private float voltageOsborn;					//	Osborn wave
+
+	// Heart timings in millisecs for the various conduction pathways
+
+	public float intervalP = 1.0f;					
+	private float durationLeftAtrium;				//	Duration for the electical output from the Left Atrium
+	private float durationRightAtrium;				//	Duration for the electrical output from the Right Atrium
+	private float durationAcrossAtria;				//	Duration for the transmission from the Right to the Left atrium						
+	private float segmentST;
+
+	private float[] intervalArrayP;                	// Array of the last 5 RR intervals from which we determine the heartrate
+
+	private float heartRate = 60.0f;
+	public float invertUWave = 2.0f;
+	public float addJWave = 0.0f;
+	public float patientMovement = 0.0f;		// Amount of patient movement
+
+
+	void Awake ()
+	{
+		intervalArrayP = new float[5];
+		triggerSANode = false;
+		triggerAVNode = false;
+	}
+
+	void Start () 
+	{
+		triggerSANode = true;		// start new heartbeat
+	}
+
+
+	void Update () 
+	{
+		if (stateSystole)					//	If the heart is stopped then stop all voltage output from all the heart's chambers
+		{
+			triggerSANode = false;			//	No atrial contraction
+			triggerAVNode = false;			//	No ventricular contraction
+			voltageP = 0.0f;
+			voltageQRS = 0.0f;
+			voltageT = 0.0f;
+			voltageU = 0.0f;
+			voltageOsborn = 0.0f;
+		}
+		else                                		//	If the heart is not stopped see if we have atrial or ventricular stimulation
+		{
+			if (triggerSANode)						// Do we need to start a new heartbeat
+			{
+				StartCoroutine (PWave());			// Trigger Atrial Contraction
+				StartCoroutine (PInterval());		// Prepare subsequent beat		
+				triggerSANode = false;
+			}
+			if (triggerAVNode)
+			{
+				StartCoroutine(QRSWave());
+				triggerAVNode = false;
+			}
+		}
+	}
+
+
+	void FixedUpdate () 		
+	{
+		voltageSumPrevious = voltageSum;
+		voltageSum = voltageP + voltageQRS + voltageT + voltageU + voltageOsborn + (patientMovement *(-0.5f + Random.value)); // total voltage across all chambers	
+	}
+
+
+	public IEnumerator PInterval () // Distance between each P wave therefore setting the heartrate - ie time to next beat
+	{
+		heartRate = 0.0f;	
+		for (var n = 0; n < 4; n++)						
+		{
+			intervalArrayP[n] = intervalArrayP [n+1];		// We move the last 4 P interval readings and ...
+			heartRate += intervalArrayP[n];					// Total all the P intervals
+		}
+		intervalArrayP[4] = intervalP;						// Add this one to it
+		heartRate += intervalArrayP[4];						// Add this one to the total
+		heartRate *= 0.2f;									// Divide the total by the number of readings to return the average over the 5 readings
+		yield return new WaitForSeconds(intervalP);			// Wait for the next pulse
+		triggerSANode = true;								//
+	}
+	
+	
+	public IEnumerator PWave ()			// Duration = 0.08 seconds - atrial contraction - relaxation hidden in QRS
+	{
+		float i = 0.0f;
+		
+		float rate = 12.5f; // rate for 0.16 secs
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageP = Mathf.SmoothStep(0.0f, 1.5f, i);
+			yield return new WaitForFixedUpdate(); 
+		}
+
+		i = 0.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageP = Mathf.SmoothStep(1.5f, 0.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+		yield return new WaitForSeconds(0.02f); 		// this is effectively the PR segment ie 0.05 to 0.12 secs
+		triggerAVNode = true;	
+	}
+	
+	public IEnumerator QRSWave ()			// Duration = 0.08 to 0.12 seconds - ventricular contraction - 0.04 + 0.08 * intervalP clamped between 0.08 and 0.12
+	{
+		float i = 0.0f;						
+			
+		float rate; // rate 
+		
+		float complexTimer;
+		
+		complexTimer = 0.08f; 				// Could Mathf.Clamp(0.04f + 0.08f * intervalP, 0.08f, 0.12f) but longer time reduceds aesthetic look for simple representation
+		rate = 25; 						
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageQRS = Mathf.SmoothStep(0.0f, -1.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		i = 0.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageQRS = Mathf.Lerp(-1.0f, 8.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+
+		//	The following line plays the audio beep that is played with every heartbeat
+
+		if (playSound)
+		{
+			this.gameObject.GetComponent<AudioSource>().Play();					//	Delete or comment out this line for no sound
+		}
+
+		//	End of audio declaration
+
+		i = 0.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageQRS = Mathf.Lerp(8.0f, -2.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		i = 0.0f;
+		
+		rate = 100.0f;
+
+		StartCoroutine (JWave());
+
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageQRS = Mathf.Lerp(-2.0f, 0.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+
+		yield return new WaitForSeconds(complexTimer * 0.5f);      // ST segment 0.08 to 0.12
+		StartCoroutine (TWave());	
+	}
+	
+	
+	public IEnumerator JWave ()
+	{
+		float i = 0.0f;
+		
+		float rate = 25.0f; // rate for 0.04 secs
+		
+		float tempCoreSqrd;
+		
+		tempCoreSqrd = addJWave * addJWave; 
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageOsborn = Mathf.SmoothStep(0.0f, 6.0f * tempCoreSqrd, i);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		i = 0.0f;
+		
+		rate = 25.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageOsborn = Mathf.SmoothStep(6.0f * tempCoreSqrd, 0.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+	
+	
+	public IEnumerator TWave ()			// Duration = 0.16 seconds - ventricular repolarisation
+	{
+		float i = 0.0f;
+		
+		float rate = 6.25f; // rate for 0.16 secs
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageT = Mathf.SmoothStep(0.0f, 2.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		i = 0.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageT = Mathf.SmoothStep(2.0f, 0.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+		StartCoroutine (UWave());
+	}
+	
+	public IEnumerator UWave ()
+	{
+		float i = 0.0f;
+		
+		float rate = 25.0f; // rate for 0.08 secs
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageU = Mathf.SmoothStep(0.0f, 2.0f - invertUWave, i);
+			yield return new WaitForFixedUpdate();
+		}
+		
+		i = 0.0f;
+		
+		while (i < 1.0f) 
+		{
+			i += Time.deltaTime * rate;			
+			voltageU = Mathf.SmoothStep(2.0f - invertUWave, 0.0f, i);
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
---
+++ unity5/Assets/Standard Assets/Effects/ECGRelease/TraceMesh.cs
@@ -0,0 +1,265 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TraceMesh : MonoBehaviour {
+
+	//	------------------------------------------
+	//	-- Declare all the variables we require --
+	//  ------------------------------------------
+
+	//	The following variables are used to describe the scripts we need to access to get the values we need to plot
+
+	private ManagerCirculation managerCirculation;
+
+	//	The following variables are used to describe how we draw the trace
+
+	private float traceHeight = 0.25f;
+	public int traceWidth = 5;							//	Time the trace runs for before restarting in whole seconds related to traceWidth
+	private float traceWidthHalved;						//	Used to offset the trace into the centre
+	public float traceLineWidth = 0.005f;				//	the line width we require
+	private float tracePlotInterval;					//	The distance between plots per sample on the horizontal axis
+	private float traceHeightOffset;					//	Distance to offset the vertices from the given mVolt height sample
+
+
+	//	The following variables are what we need to plot. They are gained by accessing the scipts detailed above
+
+	private float traceVoltage = 0.0f;					//	Detected mVolts for display
+	private float traceLastVoltage = 0.0f;				//	Detected last readings mVolts for display
+
+	//	The following variables are used to describe how we make the trace and the mesh we use to form it ...
+
+	private Mesh mesh;									//	The mesh we draw
+	private MeshFilter mf;								//	the meshfilter for the mesh we draw
+	private int traceQuadCount;							// 	Total number of quads we are making - needs to be 1 or above
+	private int traceVerticesCount;						//	The number of vertices used across the whole trace ie the length of the vertices array
+	private int traceTriangleCount;						//	The number of triangles we need to make the trace
+	private int traceNormalCount;						//	The number of normals we are using
+	private int traceQuadCounter = 0;					//	Used for the purposes of drawing the plots over time
+	private Vector3[] traceVertices;					//	Storage: Vertex positions
+	private Vector3[] traceNormals;						//	Storage: Vertex normals
+	private int[] traceTriangles;						//	Storage: Vertex triangles
+	private Color[] traceColors;						//	Storage: Vertex colors
+	public float traceColorRed = 0.0f;					//	The Red component of the Color() for the vertices
+	public float traceColorGreen = 1.0f;				//	The Green component of the Color() for the vertices
+	public float traceColorBlue = 0.0f; 				//	The Blue component of the Color() for the vertices
+	
+	//	The following variables are used to form the 4 points that will make up each quad from the two amplitude values we get from the physiological scripts
+
+	private float vectorPX;								// 	Used to calculate a vector between the quad start and end points
+	private float vectorPY;								//	""
+	private float vectorLength;							//	Used to calculate the normal
+	private float vectorNX;								//	""
+	private float vectorNY;								//	""
+	private float P1X;									//	Bottom left X position
+	private float P2X;									//	Top left X position
+	private float P3X;									//	Bottom right X position
+	private float P4X;									//	Top right X position
+	private float P1Y;									//	Bottom left Y position
+	private float P2Y;									//	Top left Y position
+	private float P3Y;									//	Bottom right Y position
+	private float P4Y;									//	top right Y position
+
+
+	//	The following are variables we use for other purposes
+
+	private int index;
+	
+	//	------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//	------------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	//	--------------------------
+	//	-- Create the functions --
+	//	--------------------------
+
+	// Used this for initialization
+
+	void Start () 
+	{
+		tracePlotInterval = 0.01f; 											//	Note we use 50 samples per second as the physiology engine uses fixed update
+		traceHeightOffset = traceLineWidth * 0.5f;							//	Calculate how far we draw the line above and beyond the baseline value per linewidth
+		traceWidthHalved = traceWidth * 0.25f;								//	Used to centre the trace on the GameObject
+
+		//mesh = GetComponent<MeshFilter>().mesh;
+
+		CreateMesh();
+		managerCirculation = GetComponent<ManagerCirculation>();
+		traceQuadCounter = 0;
+	}
+
+	
+	// Update is called once per frame
+
+	void FixedUpdate () 
+	{
+		GetTraceValues();
+		UpdateMesh();
+	}
+
+
+	//	Create the mesh and the initial quads. Note we don't weld the joins as there is no need with the amount of quads we actually draw in relation to the space we draw them in
+	
+	void CreateMesh ()
+	{
+		//	Initialise the mesh 
+
+		mesh = new Mesh();
+		mf = GetComponent<MeshFilter>();
+		mf.mesh = mesh;
+
+		//	Set up the variables
+
+		traceQuadCount = 50 * traceWidth;					//	How may quads we draw ie 50 per second that the trace cycle runs for
+		traceTriangleCount = traceQuadCount * 6;			//	How many triangles we need ie 2 triangles per quad each comprising of 6 vertices
+		traceVerticesCount = traceQuadCount * 4; 			//	Each quad needs 4 distinct vertices to describe it's position
+
+		//	Set up the arrays for the mesh (note no UVs are kept as this is a vertex lit mesh where the color is interpolation of the colors of the component triangle points)
+
+		traceVertices = new Vector3[traceVerticesCount];	//	Where we store the vertices of the trace
+		traceNormals = new Vector3[traceVerticesCount];		//	Where we store the normals (facing direction) of the vertices for the quads we are making
+		traceTriangles = new int[traceTriangleCount];		//	Where we store the triangles that make up the quads we are making
+		traceColors = new Color[traceVerticesCount];		//	Where we store the color of the vertices
+
+		//	Assign the initial values to the arrays
+
+		index = 0;
+
+		for (int n = 0; n < traceQuadCount; n++)
+		{
+			traceVertices[index] = new Vector3((tracePlotInterval * n) - traceWidthHalved, -traceHeightOffset, 0.0f);				// 	Bottom Left	
+			traceColors[index] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);										//	""
+			traceVertices[index + 1] = new Vector3((tracePlotInterval * n) - traceWidthHalved, traceHeightOffset, 0.0f);			//	Top left
+			traceColors[index + 1] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);									//	""
+			traceVertices[index + 2] = new Vector3((tracePlotInterval * (n + 1)) - traceWidthHalved,-traceHeightOffset, 0.0f);		//	Bottom Right
+			traceColors[index + 2] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);									//	""
+			traceVertices[index + 3] = new Vector3((tracePlotInterval * (n + 1)) - traceWidthHalved, traceHeightOffset, 0.0f);		//	Top Right
+			traceColors[index + 3] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);									//	""
+			index += 4;																												//	Move onto the next quad
+		}
+
+		index = 0;
+
+		for (int n = 0; n < traceQuadCount; n++)			//	How the triangles are described:	
+		{
+			traceTriangles[index] = n * 4;					// 	1 - - - - 3  	ie: 
+			traceTriangles[index + 1] = (n * 4) + 1;		// 	| .		  |		left triangle has three points: 0, 1 and 2
+			traceTriangles[index + 2] = (n * 4) + 2;		//	|   .	  |		right triangle has three points: 2, 1 and 3
+			traceTriangles[index + 3] = (n * 4) + 2;		//	|	  .   |
+			traceTriangles[index + 4] = (n * 4) + 1;		//	|	    . |		note:	
+			traceTriangles[index + 5] = (n * 4) + 3;		//	0 - - - - 2		we describe these 'clockwise'
+			
+			index += 6;
+		}
+
+		//	Now we need to assign the arrays we have made to the mesh
+		
+		mesh.vertices = traceVertices;
+		mesh.normals = traceNormals;
+		mesh.triangles = traceTriangles;
+		mesh.colors = traceColors;
+	}
+	
+	
+	//	Get the values we need to display
+	
+	void GetTraceValues ()
+	{
+		//	Access the values from the appropriate scripts
+		
+		traceVoltage = managerCirculation.voltageSum;
+		traceLastVoltage = managerCirculation.voltageSumPrevious;
+		
+		//	Scale those values for display
+		
+		traceVoltage *= 0.1f * traceHeight;
+		traceLastVoltage *=	0.1f * traceHeight;
+	}
+	
+	
+	//	Update the mesh
+	
+	void UpdateMesh ()			// TODO; maybe better to take current position - traceQuadCount and then color back from there to the present or similiar
+	{
+		traceVertices = mesh.vertices;															//	Get the current vertices
+		traceColors = mesh.colors;																//	Get the current colours
+		
+		//	Change the current readings
+		
+		vectorPX = traceVoltage - traceLastVoltage;												//	Calculate the perpendicular to the vector for the start and end points
+		vectorPY = -tracePlotInterval;															//	""
+		vectorLength = Mathf.Sqrt((vectorPX * vectorPX) + (vectorPY * vectorPY));				//	Then normalise that vector	
+		vectorNX = vectorPX / vectorLength;														//	""
+		vectorNY = vectorPY / vectorLength;														//	""
+		
+		P1X = (tracePlotInterval * traceQuadCounter) + (vectorNX * traceHeightOffset) - traceWidthHalved;			//	Calculate the 4 points for the quad by
+		P1Y = (traceLastVoltage) + (vectorNY * traceHeightOffset);													//	adding normalised vector and multiplying
+		P2X = (tracePlotInterval * traceQuadCounter) - (vectorNX * traceHeightOffset) - traceWidthHalved;			//	it by half the line width we require.
+		P2Y = (traceLastVoltage) - (vectorNY * traceHeightOffset);													//	""
+		P3X = (tracePlotInterval * (traceQuadCounter + 1)) + (vectorNX * traceHeightOffset) - traceWidthHalved;		//	""
+		P3Y = (traceVoltage) + (vectorNY * traceHeightOffset);														//	""
+		P4X = (tracePlotInterval * (traceQuadCounter + 1)) - (vectorNX * traceHeightOffset) - traceWidthHalved;		//	""
+		P4Y = (traceVoltage) - (vectorNY * traceHeightOffset);
+		
+		traceVertices[traceQuadCounter * 4] = new Vector3(P1X, P1Y, 0.0f);						// 	Bottom Left
+		traceVertices[traceQuadCounter * 4 + 1] = new Vector3(P2X, P2Y, 0.0f);					//	Top left
+		traceVertices[traceQuadCounter * 4 + 2] = new Vector3(P3X, P3Y, 0.0f);					//	Bottom Right
+		traceVertices[traceQuadCounter * 4 + 3] = new Vector3(P4X, P4Y, 0.0f);					//	Top Right
+		traceColors[traceQuadCounter * 4] = new Color(1, 1, 1, 1);								// 	Bottom Left
+		traceColors[traceQuadCounter * 4 + 1] = new Color(1, 1, 1, 1);							//	Top left
+		traceColors[traceQuadCounter * 4 + 2] = new Color(1, 1, 1, 1);							//	Bottom Right
+		traceColors[traceQuadCounter * 4 + 3] = new Color(1, 1, 1, 1);							//	Top Right
+		
+		//	Change the color on the previous quad to the standard trace colour
+		
+		if (traceQuadCounter - 1 < 0)															//	The first drawn quad on the line previous entry is the last drawn quad on the lin
+		{
+			traceColors[(traceQuadCount - 1) * 4] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[((traceQuadCount - 1) * 4) + 1] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[((traceQuadCount - 1) * 4) + 2] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[((traceQuadCount - 1) * 4) + 3] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+		}
+		else                                                                                    //  The other quads don't need this adjustment
+		{
+			traceColors[(traceQuadCounter - 1) * 4] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[(traceQuadCounter - 1) * 4 + 1] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[(traceQuadCounter - 1) * 4 + 2] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+			traceColors[(traceQuadCounter - 1) * 4 + 3] = new Color(traceColorRed, traceColorGreen, traceColorBlue, 1);
+		}
+
+		//	Set up a color gradient on the quads in front of the current reading to show trace fading
+		
+		index = traceQuadCounter + 1;
+
+		//float fadeConstant  = 1 / (traceWidth * 1);
+		
+		for (int n = 0; n < traceWidth * 10.0f; n++)												//	We start the fading out "10 multiplied by the cycle duration secs-1" quads ahead
+		{
+			if (index  + 1 > traceQuadCount)
+			{
+				index = 0;
+			}
+			
+			float fadeColor = Mathf.Clamp(n * 0.1f - 1.0f, 0.0f, 1.0f);								//	And we fade from our main trace colour towards black
+
+			traceColors[index * 4] = new Color(traceColorRed, traceColorGreen, traceColorBlue, fadeColor);
+			traceColors[index * 4 + 1] = new Color(traceColorRed, traceColorGreen, traceColorBlue, fadeColor);
+			traceColors[index * 4 + 2] = new Color(traceColorRed, traceColorGreen, traceColorBlue, fadeColor);
+			traceColors[index * 4 + 3] = new Color(traceColorRed, traceColorGreen, traceColorBlue, fadeColor);
+			
+			index ++;
+		}
+		
+		traceQuadCounter ++;
+		
+		if (traceQuadCounter  + 1 > traceQuadCount)
+		{
+			traceQuadCounter = 0;
+		}
+		
+		//	Apply all the change data back to the mesh 
+		
+		mesh.vertices = traceVertices;
+		mesh.colors = traceColors;
+	}
+}
+
+
