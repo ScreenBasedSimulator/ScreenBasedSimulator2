e43ee468e522ce8d7c16ae8876ff60317c6bffbe
Jiaqing Qu <jiaqingq@andrew.cmu.edu>
2016-02-17 03:04:02 -0500

    Nurse Walk Animation

---
+++ unity5/Assets/ControlAneStore.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlAneStore : MonoBehaviour {
+
+	Animator animator;
+	
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Return)){
+						print("Return on ane store has been pressed");
+			animator.SetBool("begin_move", true);
+		}
+	}
+}
