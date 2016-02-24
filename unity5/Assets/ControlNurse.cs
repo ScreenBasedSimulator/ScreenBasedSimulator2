8516598b02d0cea4776cf0ca584615467c6b95c0
Jiaqing Qu <jiaqingq@andrew.cmu.edu>
2016-02-23 23:03:38 -0500

    Nurse walk in animation updated

--- unity5/Assets/ControlNurse.cs
+++ unity5/Assets/ControlNurse.cs
@@ -2,22 +2,31 @@
 using System.Collections;
 
 public class ControlNurse : MonoBehaviour {
-	
+
+	GameObject nurse;
+	GameObject aneStore;
+
 	Animator animator;
 	Animator animator2;
 	
 	// Use this for initialization
 	void Start () {
+		nurse = GameObject.Find("/Characters/Surgeon_C_OR");
+		aneStore = GameObject.Find ("/Props/Props_Big/AneStore");
+
 		animator = GetComponent<Animator>();
-		animator2 = GameObject.Find ("/Props/Props_Big/AneStore").GetComponent<Animator> ();
+		animator2 = aneStore.GetComponent<Animator> ();
+
+		nurse.transform.localScale = new Vector3(0, 0, 0);
+		aneStore.transform.localScale = new Vector3 (0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.Return)){
-			//			print("Return has been pressed");
+			nurse.transform.localScale = new Vector3(1, 1, 1);
+			aneStore.transform.localScale = new Vector3 (1, 1, 1);
 			animator.SetBool("begin_walk", true);
-//			print (animator2.avatar);
 			animator2.SetBool("begin_move", true);
 		}
 	}
