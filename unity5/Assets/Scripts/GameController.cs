578fc68458ba7975269c9e6f66a975b0103de9d8
CodeMySky <codemysky@hotmail.com>
2016-03-01 09:56:42 -0500

    try to fix animation

---
+++ unity5/Assets/Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject nurse;
+	public GameObject aneStore;
+
+	Animator nurseAnimator;
+	Animator aneStoreAnimator;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		nurseAnimator = nurse.GetComponent<Animator> ();
+		aneStoreAnimator = aneStore.GetComponent<Animator> ();
+
+		// Hide nurse and aneStore at the beginning
+		nurse.transform.localScale = new Vector3 (0, 0, 0);
+		aneStore.transform.localScale = new Vector3 (0, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.Return)) {
+			// Show the nurse and the aneStore
+			nurse.transform.localScale = new Vector3 (1, 1, 1);
+			aneStore.transform.localScale = new Vector3 (1, 1, 1);
+
+			nurseAnimator.SetBool ("begin_walk", true);
+			aneStoreAnimator.SetBool ("begin_move", true);
+		}
+	}
+}
---
+++ unity5/Assets/Scripts/PatientController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatientController : MonoBehaviour
+{
+	private string stringToEdit = "啊你居然点击了病人";
+	private GUIText mText;
+	private bool isClick;
+	private string prevText;
+	void Start ()
+	{
+	
+	}
+
+
+	void Update ()
+	{
+		Ray mRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit mHi;
+		if (Physics.Raycast (mRay, out mHi)) {
+			if (prevText != mHi.collider.gameObject.tag) {
+				prevText = mHi.collider.gameObject.tag;
+				Debug.Log (prevText);
+			}
+
+			if (mHi.collider.gameObject.tag == "Patient") {
+				Debug.Log ("hehe");
+				isClick = true;
+			} else {
+				isClick = false;
+			}
+
+		}
+	}
+
+
+	void OnGUI ()
+	{
+		if (isClick) {
+
+			GUI.Label (new Rect (500, 100, 300, 100), "what do you want to do!!!!!!!");
+			if (GUI.Button (new Rect (500, 150, 100, 100), "kill him"))
+				Debug.Log ("Clicked the button with an image");
+		}
+	}
+}
