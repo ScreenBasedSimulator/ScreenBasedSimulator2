54ea9e473537461aac4474bde8d2698f45af646d
Ava Huang <xinlyuh@andrew.cmu.edu>
2016-03-01 14:46:38 -0500

    light and mask

---
+++ unity5/Assets/maskOnOff.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class maskOnOff : MonoBehaviour {
+	bool flag;
+	GameObject gameObject;
+	// Use this for initialization
+	void Start () {
+		flag = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("putMask")){
+			gameObject.GetComponent<Renderer>().enabled = flag;
+			flag = flag ? false: true;
+		}
+	}
+}
